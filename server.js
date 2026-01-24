bbcb62a99487d676c6643091e02d3a479c270c37
Jacobus Verhagen <kverhagen@mac.com>
2026-01-24 09:42:35 +0000

    Increase request body limit to fix damage report PDF upload

--- server.js
+++ server.js
@@ -75,11 +75,9 @@ const PUBLIC_API_BASE = "https://api.equinetransportuk.com";
 
 const app = express();
 
-// 🔑 THIS MUST EXIST
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
-// (optional but fine)
-app.use(express.urlencoded({ extended: true }));
 
 
 app.use((req, res, next) => {
